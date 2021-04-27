3e33fdb1121811785b9b913fe05739177fa4fd81
1hodo <43621754+1hodo@users.noreply.github.com>
2021-04-27 12:45:53 -0700

    u

---
+++ script.js
@@ -0,0 +1,15 @@
+// dom event 1 and function 1
+function changeBackground() {
+    //dom query 1 
+     //update page content
+    document.getElementsByClassName("item3")[0].style.background = "url(peace.gif) no-repeat";
+    document.getElementsByClassName("item3")[0].style.backgroundSize = "100% 100%";
+}
+
+//dom event 2 and function 2
+function changeColor() { 
+    //dom query 2 
+    //update css property 
+    document.getElementById("colorChange").style.color = "cyan";
+}
+
